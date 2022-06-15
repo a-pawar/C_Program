0c4749d4b9603e3c8e521a88876271430cc44810
Anshul Pawar <99128628+a-pawar@users.noreply.github.com>
2022-06-15 17:45:47 +0530

    Add files via upload

---
+++ 1.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+
+int main(){
+  printf("Name=Dolly narware\n");
+  printf("Student ID = 30378\n");
+  printf("department=Electrinic and communication Engineering\n");
+  return 0;
+}
