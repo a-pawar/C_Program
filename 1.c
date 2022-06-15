7c0e3713506038b8d11f54f0a7575d4b57dcd162
Anshul Pawar <99128628+a-pawar@users.noreply.github.com>
2022-06-15 17:52:43 +0530

    Delete 1.c

--- 1.c
+++
@@ -1,8 +0,0 @@
-#include<stdio.h>
-
-int main(){
-  printf("Name=Dolly narware\n");
-  printf("Student ID = 30378\n");
-  printf("department=Electrinic and communication Engineering\n");
-  return 0;
-}
