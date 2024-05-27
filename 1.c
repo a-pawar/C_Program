1dc82bdf0053b4bbb96b8661b736e8ee8bb93ab9
UChourasiya13 <ujjwalchourasiya40287@gmail.com>
2024-05-27 20:59:50 +0530

    commitUpdate

--- 1.c
+++ 1.c
@@ -3,11 +3,9 @@
 int main()
 {
   // printing student information
-
   printf("Name=Anshul Pawar\n");
   printf("Student ID = De20020\n");
   printf("department=Information Technology\n");
   printf("college=IIPS\n");
-
   return 0;
 }
